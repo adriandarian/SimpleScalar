19bc6d16efb6912e12a01a90ba8eb6c3eaa1e1fb
adriandarian <adarian@ucmerced.edu>
2020-12-14 00:35:33 -0800

    fix: missing variables in test file

--- build/spec2000args/test.sh
+++ build/spec2000args/test.sh
@@ -2,12 +2,13 @@
 # Spec2000 Binary #
 ###################
 
-# Pre-Requisite: source ~/.bashrc
-
-# MAX:INSTANCE
-inst=50000000
-# FAST:FORWARD
-ff=20000000
+inst=50000000 # MAX:INSTANCE
+ff=20000000 # FAST:FORWARD
+SSDIR=/root/build/simplesim-3.0 # Simple Sim 3.0 Directory
+BENCHDIR=/root/benchmarks # Benchmarks Directory
+SBDIR=/root/build/spec2000binaries # Spec 2000 Binaries Directory
+SADIR=/root/build/spec2000args # Spec 2000 Arguments Directory
+RSDIR=/root/build/runscripts # Run Scripts Directory
 
 for path in /root/build/spec2000args/*; do
     [ -d "${path}" ] || continue # if not a directory, skip
